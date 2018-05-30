c223264628b9562c263dc870aa53563cd6671c3a
lorenzek <lorenzek@purdue.edu>
2018-05-30 16:22:20 -0400

    more formatting

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-function on() {
-    document.getElementById("overlay").style.display = "block";
-}
+// function on() {
+//     document.getElementById("overlay").style.display = "block";
+// }
 
-function off() {
-    document.getElementById("overlay").style.display = "none";
-}
+// function off() {
+//     document.getElementById("overlay").style.display = "none";
+// }
