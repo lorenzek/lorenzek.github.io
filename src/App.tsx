ef726105dff18b210d2cdda8540c876d6a1e2c7b
lorenzek <lorenzek253@gmail.com>
2023-01-16 15:17:40 -0600

    Setting up react

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css';
+import React from 'react'
+import { Route, Routes } from 'react-router-dom';
+
+const App = () =>  {
+  return (
+  <Routes>
+    <Route></Route>
+  </Routes>
+  );
+}
+
+export default App;
